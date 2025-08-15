b0b15036ab639089d687e0505d5460e5d83a9525
David Betancourt <dmarquez.develop@gmail.com>
2025-08-15 02:38:25 +0000

    could not add another child to parent

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+
 
 db = SQLAlchemy()
 
@@ -17,3 +19,78 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Likes(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(String(120), unique=True, nullable=False)
+    post_id: Mapped[int] = mapped_column(nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+class Followers(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    from_user_id: Mapped[int] = mapped_column(String(120), unique=True, nullable=False)
+    to_user_id: Mapped[int] = mapped_column(nullable=False)
+    
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+    
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    text: Mapped[str] = mapped_column(nullable=False)
+    username_id: Mapped[int] = mapped_column(nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    users: Mapped["Users"] = relationship(back_populates="post")
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text
+        }	
+
+class Comments(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(String(120), unique=True, nullable=False)
+    post_id: Mapped[int] = mapped_column(nullable=False)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    users: Mapped["Users"] = relationship(back_populates="comments")
+    
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+    
+class Users(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firstname: Mapped[str] = mapped_column(Boolean(), nullable=False)
+    lastname: Mapped[str] = mapped_column(Boolean(), nullable=False)
+
+    posts: Mapped[List["Post"]] = relationship(back_populates="users")
+    comments: Mapped[List["Comments"]] = relationship(back_populates="users")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username
+            # do not serialize the password, its a security breach
+        }
+    
+			    		
